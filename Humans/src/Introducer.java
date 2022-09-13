ebfe94f4c6912824aa2f29548ad97045a205cfbf
1005136001 <1005136001@L829WD210022.edu.cbe.ab.ca>
2022-09-13 14:05:21 -0600

    commit

--- Humans/src/Introducer.java
+++ Humans/src/Introducer.java
@@ -1,50 +1,59 @@
 import java.util.Random;
 public class Introducer {
 	public String createPublicIntroduction(Human human) {
+		
 		String introduction = String.format("I am pleased to introduce %s %s.", human.getFirstName(), human.getLastName());
-		String pronoun = "They";
-		switch(human.getGender()) {
-		case MALE:
-			pronoun = "He";
-			break;
-		case FEMALE:
-			pronoun = "She";
-			break;
-		case GENDER_FLUID:
-			Random x = new Random();
-			if(x.nextBoolean()) {
+		try {
+			
+			String pronoun;
+			switch(human.getGender()) {
+			case MALE:
 				pronoun = "He";
-			}
-			else {
+				break;
+			case FEMALE:
 				pronoun = "She";
-				
-			}
-			break;
-		default:
-			pronoun = "They";
-			break;
-		}
-			
-		if(human instanceof Adult) {
-			Adult adult = (Adult) human;
-			introduction = String.format("%s %s works at %s and their occupation is %s.", introduction, pronoun, adult.getPlaceOfWork(), adult.getOccupation() );
-		}	
-		else if(human instanceof Youth) {
-			Youth youth = (Youth) human;
-			introduction = String.format("%s %s goes to %s and is in grade %d.", introduction, pronoun, youth.getSchoolName(), youth.getSchoolGrade());
-			
-			if(youth instanceof WilliamAberhartStudent) {
-				WilliamAberhartStudent abeStudent = (WilliamAberhartStudent) human;
-				try {
-					abeStudent.getHomeRoomTeacher();
-					introduction = String.format("%s %s belongs to %s's homeroom, which is in room %d.",introduction, pronoun, abeStudent.getHomeRoomTeacher(), abeStudent.getHomeRoom());
+				break;
+			case GENDER_FLUID:
+				Random x = new Random();
+				if(x.nextBoolean()) {
+					pronoun = "He";
 				}
-				catch(NullPointerException e) {
-			
+				else {
+					pronoun = "She";
+					
 				}
+				break;
+			default:
+				pronoun = "They";
+				break;
+			}
 				
+			if(human instanceof Adult) {
+				Adult adult = (Adult) human;
+				introduction = String.format("%s %s works at %s and their occupation is %s.", introduction, pronoun, adult.getPlaceOfWork(), adult.getOccupation() );
+			}	
+			else if(human instanceof Youth) {
+				Youth youth = (Youth) human;
+				introduction = String.format("%s %s goes to %s and is in grade %d.", introduction, pronoun, youth.getSchoolName(), youth.getSchoolGrade());
+				
+				if(youth instanceof WilliamAberhartStudent) {
+					WilliamAberhartStudent abeStudent = (WilliamAberhartStudent) human;
+					
+					
+					String homeRoomTeacher = abeStudent.getHomeRoomTeacher();
+					if (homeRoomTeacher == null) {
+						introduction = String.format("%s Their homeroom is unknown at this time.",introduction);
+					}
+					introduction = String.format("%s %s belongs to %s's homeroom, which is in room %d.",introduction, pronoun, homeRoomTeacher, abeStudent.getHomeRoom());
+					
+				}
 			}
 		}
+		catch(NullPointerException e) {
+			
+		}
+		
+		
 		return introduction;
 	}
 }
