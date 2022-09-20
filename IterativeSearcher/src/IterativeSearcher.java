1120ca99e07f33ea7d1d631fa8e807c6e40bd5f0
1005136001 <1005136001@L829WD210022.edu.cbe.ab.ca>
2022-09-20 15:04:45 -0600

    commit

--- IterativeSearcher/src/IterativeSearcher.java
+++ IterativeSearcher/src/IterativeSearcher.java
@@ -2,6 +2,20 @@
 public class IterativeSearcher {
 
 	public int doBinarySearch(IntegerArray arrayToSort, int i) {
+		int x = arrayToSort.length() / 2;
+		for (int j = 0; j < arrayToSort.length(); j++) {
+			int y = arrayToSort.read(x);
+			if(i < y) { 
+				x = x - (x/2);
+			}
+			else if (i > y) {
+				x = x + (x/2);
+			}
+			else { 
+				return x; 
+			}
+		}
+		return 0;
 	}
 
 }
