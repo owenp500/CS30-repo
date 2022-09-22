8821de67b3b288f126f9bd0f1a8994a8f5a35a9a
1005136001 <1005136001@L829WD210022.edu.cbe.ab.ca>
2022-09-22 15:03:20 -0600

    commit changed everything

--- IterativeSearcher/src/IterativeSearcher.java
+++ IterativeSearcher/src/IterativeSearcher.java
@@ -1,15 +1,42 @@
-import java.lang.Math;
+
 public class IterativeSearcher {
 
-	public int doBinarySearch(IntegerArray arrayToSort, int i) {
-		int length = arrayToSort.length();
+	public int doBinarySearch(IntegerArray arrayToSearch, int i) {
+		int low = 0;
+		int high = arrayToSearch.length();
+		while (low < high) {
+			int mid = (low + high)/2;
+			int j = arrayToSearch.read(mid);
+			if (i == j) {
+				for(int x = 0; x < (mid - low); x++) {
+					if (arrayToSearch.read(mid - x - 1) != i) {
+						return mid - x;
+					}
+				}
+			}
+			else if(i > j) {
+				low = mid + 1;
+			}
+			else {
+			high = mid - 1;
+			}
+		}
+		return - 1;
+		
+	}
+}
+	/*
+int length = arrayToSort.length();
 		int stake = ((length) / 2);
 		
 		for (int j = 0; j < length; j++) {
 			int y = arrayToSort.read(stake);
+			
 			int difference = ((Math.floor(length/Math.pow(2,(j + 2)))) == 0) ? 1: 
 				(int) (length/Math.pow(2,(j + 2)));
-			
+			if (Math.floor(length/Math.pow(2, j)) == 0) {
+				difference = 1;
+			}
 			if(i < y) { 
 				stake -= difference;
 			}
@@ -22,5 +49,4 @@ public class IterativeSearcher {
 		}
 		return -1;
 	}
-
-}
+*/
