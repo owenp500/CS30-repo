3b9d7043f3ef13aaf33d6aebd0a1cab14980c581
1005136001 <1005136001@L829WD210022.edu.cbe.ab.ca>
2022-09-27 14:32:36 -0600

    New Project: IterativeSorter

---
+++ IterativeSorter/src/IterativeSorter.java
@@ -0,0 +1,12 @@
+public class IterativeSorter {
+	
+	public void doBubbleSort(IntegerArray arrayToSort) {
+	}
+
+	public void doSelectionSort(IntegerArray arrayToSort) {
+	}
+
+	public void doInsertionSort(IntegerArray arrayToSort) {
+	}
+
+}
