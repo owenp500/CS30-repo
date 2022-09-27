1bc6fddf002fe9feed40998763ce0ceaa25aada0
1005136001 <1005136001@L829WD210022.edu.cbe.ab.ca>
2022-09-27 15:04:06 -0600

    commit

--- IterativeSorter/src/IterativeSorter.java
+++ IterativeSorter/src/IterativeSorter.java
@@ -1,6 +1,17 @@
 public class IterativeSorter {
 	
 	public void doBubbleSort(IntegerArray arrayToSort) {
+		int i = arrayToSort.length();
+		boolean sorted = false;
+		while ((i > 1) && (!sorted)) {
+			sorted = true;
+			for(int j = 1; j < 1; j++) {
+				if (arrayToSort.read(j-1) > arrayToSort.read(j)) {
+					
+				}
+			}
+		}
+	
 	}
 
 	public void doSelectionSort(IntegerArray arrayToSort) {
