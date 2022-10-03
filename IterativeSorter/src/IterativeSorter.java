06c544d2aba42cde5e47d2330dc11708e186218c
1005136001 <1005136001@L829WD210022.edu.cbe.ab.ca>
2022-10-03 13:41:33 -0600

    commit

--- IterativeSorter/src/IterativeSorter.java
+++ IterativeSorter/src/IterativeSorter.java
@@ -20,22 +20,32 @@ public class IterativeSorter {
 		int length = arrayToSort.length();
 		
 		  for(int i = 0; i < length; i++) {
-			  int minValue = arrayToSort.read(i);
+			  int firstValue =  arrayToSort.read(i);
+			  int minValue = firstValue;
 			  int nextRead;
 			  int indexMinValue = i;
-			 for(int j = 1; j < length; j++) {
+			 for(int j = i; j < length; j++) {
 				nextRead = arrayToSort.read(j);
 				if(minValue > nextRead) {
 					minValue = nextRead;
 					indexMinValue = j;
 				}
-			 }
+			 } 
 			 arrayToSort.write(i,minValue);
-			 arrayToSort.write(indexMinValue, nextRead);
+			 arrayToSort.write(indexMinValue, firstValue);
 		  }
 	}
 
 	public void doInsertionSort(IntegerArray arrayToSort) {
+		int length = arrayToSort.length();
+		for(int i = 0; i < length; i++) {
+			  
+			 for(int j = i; j < length; j++) {
+				
+				
+			 } 
+			 
+		  }
 	}
 	
 	private void swap(int indexA, int indexB, IntegerArray arrayToSwap) {
