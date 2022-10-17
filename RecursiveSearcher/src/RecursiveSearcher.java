56d8e4f793132211974352bf38a8f9097c2b17ee
1005136001 <1005136001@L829WD210022.edu.cbe.ab.ca>
2022-10-17 13:41:41 -0600

    commit StackOverFlow error caued by looking for the first instance of target

--- RecursiveSearcher/src/RecursiveSearcher.java
+++ RecursiveSearcher/src/RecursiveSearcher.java
@@ -1,12 +1,29 @@
 
 public class RecursiveSearcher {
-	
-	public RecursiveSearcher() {
+	public RecursiveSearcher() {		
 	}
 
 	public int doBinarySearch(IntegerArray arrayOfInts, int target) {
-		
-		return 0;
+		int high = arrayOfInts.length();
+		return doBinarySearch(arrayOfInts, target, 0, high);
+	}
+	private int doBinarySearch(IntegerArray arrayOfInts, int target, int low,int high) {
+		int mid  = (high + low) / 2;
+		int midValue = arrayOfInts.read(mid);
+		if( midValue == target) {
+			int nextValue = (doBinarySearch(arrayOfInts, target, mid -1, mid -1));
+			if (nextValue == -1) {
+				return mid;
+			}
+			else { return nextValue; }
+		}
+		else if(midValue < target) {
+			return doBinarySearch(arrayOfInts, target, mid, high);
+		}
+		else if (midValue >target) {
+			return  doBinarySearch(arrayOfInts, target, low, mid); 
+		}
+		return -1;
 	}
 	
 }
