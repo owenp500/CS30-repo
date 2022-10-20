4f9f0ffe2f893b4d4d6e37d487e7632d7bb4a2f9
1005136001 <1005136001@L829WD210022.edu.cbe.ab.ca>
2022-10-20 15:04:15 -0600

    commit

--- RecursiveSorter/src/RecursiveSorter.java
+++ RecursiveSorter/src/RecursiveSorter.java
@@ -2,6 +2,9 @@ public class RecursiveSorter {
 	
 	public void doQuickSort(IntegerArray clone) {
 		int pivot = clone.read(clone.length());
+		for(int i = 0; i < clone.length(); i++) {
+			if( )
+		}
 	}
 
 	public void doMergeSort(IntegerArray array) {
