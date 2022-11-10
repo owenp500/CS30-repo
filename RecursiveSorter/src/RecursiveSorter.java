a00e066c67dd88130fa6267eb7dcff110e0508da
owenp500 <opringle9@gmail.com>
2022-11-10 15:01:39 -0700

    commit

--- RecursiveSorter/src/RecursiveSorter.java
+++ RecursiveSorter/src/RecursiveSorter.java
@@ -40,9 +40,53 @@ public class RecursiveSorter {
 			doQuickSort(clone, low, right - 1);
 		}
 	}
-
+	
 	public void doMergeSort(IntegerArray array) {
+		//check for base case
+		
+		//create two sub arrays
+		
+		//doMergeSort recursively until length of one
+		
+		//coming out of recursion doMerge function all the way up the stacks
+		
 		
 	}
-
+	public int[] doMerge(int[] a, int[] b) {
+		if (a == null && b == null) {
+			return new int[0];
+		}
+		else if (a == null) {
+			return b;
+		}
+		else if(b == null) {
+			return a;
+		}
+		int[] ab = new int[a.length + b.length];	
+		int iA = 0;
+		int iB = 0;
+		int j = 0;
+		while (iA < a.length && iB < b.length) {
+			if(a[iA] < b[iB]) {
+				ab[j] = a[iA];
+				iA++;
+			}
+			else {
+				ab[j] = b[iB];
+				iB++;
+			}
+			j++;
+		}
+		while (iA < a.length) {
+			ab[j] = a[iA];
+			iA++;
+			j++;
+		}
+		while (iB < b.length) {
+			ab[j] = b[iB];
+			iB++;
+			j++;
+		}
+		return ab;
+	}
 }
