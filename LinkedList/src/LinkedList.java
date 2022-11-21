618ecfa67504089013e3feea4dc88a12baad46ff
owenp500 <opringle9@gmail.com>
2022-11-21 13:43:43 -0700

    commit

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -15,27 +15,54 @@ public class LinkedList {
 	
 	public void add(int i, String string) {
 		if(size == 0) {
-			firstNode = new Node("String", null, null);
+			firstNode = new Node(string, null, null);
 			previousNode = firstNode;
 		}
 		else {
 			Node targetNode = firstNode;
-			for(int j = 0; j < i &&  j < size; j++) {
+			for(int j = 0; j < i - 1; j++) {
 				targetNode = targetNode.next;
 			}
-			
+			targetNode.next = new Node(string, targetNode, null);
 		}
 		size++;
-		//change pointers of neighbour and node being replaced to this node
+
 		
+	}
+	public String toString() {
+		String string = "[" + firstNode.value;
+		Node targetNode = firstNode.next;
+		for (int i = 1; i < size; i++) {
+			string +=", " + targetNode.value  ;
+			targetNode = targetNode.next;
+		}
+		string += "]";
+		
+		return string;
 	}
 
 	public String get(int i) {
-		return null;
+		Node targetNode = firstNode;
+		for (int j = 0; j < i; j++) {
+			targetNode = targetNode.next;
+		}
+		return targetNode.value;
 	}
 
 	public void remove(int i) {
 		//removing a node will set the neighbour's pointers to each other
+		Node targetNode = firstNode;
+		for (int j = 0; j < i - 1; j++) {
+			targetNode = targetNode.next;
+		}
+		if(targetNode.previous != null) {
+			targetNode.previous.next = targetNode.next;
+		}
+		if(targetNode.next != null) {
+			targetNode.next.previous = targetNode.previous;
+		}
+
+
 		size--;
 	}
 
