3b279b267f3570238ceba40daf719b5f1d3b89b0
Owen Pringle <opringle9@gmaicom>
2022-11-23 20:47:37 -0800

    commit

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -2,20 +2,16 @@ public class LinkedList {
 	int size = 0;
 	Node firstNode;
 	
-	
-	
 	public int size() {
 		return size;
 	}
 	public void add(String string) {
 		add(size, string);
 	}
-	
 	public void add(int i, String string) {
 		if(i > size || i < 0) {
 			throw new IndexOutOfBoundsException();
-		}
-		
+		}	
 		else {
 			if(size == 0) {
 				firstNode = new Node(string, null, null);
@@ -33,21 +29,22 @@ public class LinkedList {
 				targetNode.next = new Node(string, targetNode, null);
 			}
 			size++;
-		}
-		
+		}		
 	}
 	public String toString() {
-		String string = "[" + firstNode.getValue();
-		Node targetNode = firstNode.next;
-		for (int i = 1; i < size; i++) {
-			string +=", " + targetNode.value  ;
-			targetNode = targetNode.next; 
+		String string = "[";
+		if(firstNode != null) {
+			string+= firstNode.getValue();
+			Node targetNode = firstNode.next;
+			for (int i = 1; i < size; i++) {
+				string +=", " + targetNode.value  ;
+				targetNode = targetNode.next; 
+			}
 		}
 		string += "]";
 		
 		return string;
 	}
-
 	public String get(int i) {
 		Node targetNode = firstNode;
 		for (int j = 0; j < i; j++) {
@@ -57,7 +54,6 @@ public class LinkedList {
 	}
 
 	public void remove(int i) {
-		//removing a node will set the neighbour's pointers to each other
 		Node targetNode = firstNode;
 		for (int j = 0; j < i; j++) {
 			targetNode = targetNode.next;
@@ -81,8 +77,10 @@ public class LinkedList {
 	}
 
 	public void set(int i, String string) {
-		//This should only have to change the value at the given node not create a new node
-	}
-
-	
+		Node targetNode = firstNode;
+		for(int j = 0; j < i; j++) {
+			targetNode = targetNode.next;
+		}
+		targetNode.value = string;
+	}	
 }
